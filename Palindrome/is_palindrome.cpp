c5bc050a9871f5329a60c750c0af4289248c5d27
Dachanelle <57224657+Dachanelle@users.noreply.github.com>
2020-11-17 18:45:21 -0500

    Create is_palindrome.cpp
    Returns true if the text passes is a palindrome. Palindromes are words that are the same backward and forward.

---
+++ Palindrome/is_palindrome.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+// Define is_palindrome() here:
+bool is_palindrome(std::string text){
+  std::string reverse;
+
+  for(int i=text.size(); i>0; i--){
+    reverse = text[i];
+  }
+  if(reverse == text){
+    return true;
+  }else {
+    return false;
+  }
+}
+
+
+int main() {
+  
+  std::cout << is_palindrome("madam") << "\n";
+  std::cout << is_palindrome("ada") << "\n";
+  std::cout << is_palindrome("lovelace") << "\n";
+  
+}
